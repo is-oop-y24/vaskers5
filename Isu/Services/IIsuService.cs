2b7821dc0e894ffff306b7e3974afe1908751763
vaskers5 <danil.krokodil@mail.ru>
2021-09-19 12:33:00 +0300

    delete blank lines

--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -6,15 +6,12 @@ namespace Isu.Services
      {
          Group AddGroup(string name);
          Student AddStudent(Group @group, string name);
-
          Student GetStudent(int id);
          Student FindStudent(string name);
          List<Student> FindStudents(string groupName);
          List<Student> FindStudents(int courseNumber);
-
          Group FindGroup(string groupName);
          List<Group> FindGroups(int courseNumber);
-
          void ChangeStudentGroup(Student student, Group newGroup);
      }
 }
