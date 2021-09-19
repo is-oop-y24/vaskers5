500f2c3ed846f48ece79ff5bab7d557c5b7f477b
vaskers5 <danil.krokodil@mail.ru>
2021-09-19 12:43:35 +0300

    fixed mistake with "{"

--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -2,14 +2,15 @@ using System.Collections.Generic;
 
 namespace Isu.Services
 {
-     public interface IIsuService
-     {
-         Group AddGroup(string name);
-         Student AddStudent(Group @group, string name);
-         Student GetStudent(int id);
-         Student FindStudent(string name);
-         List<Student> FindStudents(string groupName);
-         List<Student> FindStudents(int courseNumber);
-         Group FindGroup(string groupName);
-         List<Group> FindGroups(int courseNumber);
+    public interface IIsuService
+    {
+        Group AddGroup(string name);
+        Student AddStudent(Group @group, string name);
+        Student GetStudent(int id);
+        Student FindStudent(string name);
+        List<Student> FindStudents(string groupName);
+        List<Student> FindStudents(int courseNumber);
+        Group FindGroup(string groupName);
+        List<Group> FindGroups(int courseNumber);
+    }
 }
