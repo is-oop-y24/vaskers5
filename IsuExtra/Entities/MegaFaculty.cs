bb1532cbf4f30e82e4543748e4bb81c660ee2807
vaskers5 <danil.krokodil@mail.ru>
2021-11-07 23:41:33 +0300

    change type of property

--- IsuExtra/Entities/MegaFaculty.cs
+++ IsuExtra/Entities/MegaFaculty.cs
@@ -2,13 +2,13 @@ namespace IsuExtra.Entities
 {
     public class MegaFaculty
     {
-        public MegaFaculty(string facultySymbol, string facultyName)
+        public MegaFaculty(char facultySymbol, string facultyName)
         {
             FacultySymbol = facultySymbol;
             FacultyName = facultyName;
         }
 
-        public string FacultySymbol { get; set; }
+        public char FacultySymbol { get; set; }
         public string FacultyName { get; set; }
     }
 }
