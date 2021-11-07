3d00d4e8d45c858c2b3b959ffbd919617f13c2b0
vaskers5 <danil.krokodil@mail.ru>
2021-11-07 23:40:45 +0300

    refactore student

--- IsuExtra/Entities/StudentWithTimeTable.cs
+++ IsuExtra/Entities/StudentWithTimeTable.cs
@@ -3,15 +3,19 @@ using Isu.Services;
 
 namespace IsuExtra.Entities
 {
-    public class StudentWithTimeTable
+    public class StudentWithTimeTable : Student
     {
-        public StudentWithTimeTable(Student student, List<Lesson> timeTable)
+        public StudentWithTimeTable(int id, string name, ExtraGroup group)
+            : base(id, name, group)
         {
-            OgnpStudent = student;
-            Timetable = timeTable;
+            Timetable = group.TimeTable;
+            OgnpSubjects = new List<EducationCourse>() { };
+            FacultySymbol = group.GroupName[0];
         }
 
-        public Student OgnpStudent { get; set; }
+        public List<EducationCourse> OgnpSubjects { get; set; }
+
         public List<Lesson> Timetable { get; set; }
+        public char FacultySymbol { get; }
     }
 }
