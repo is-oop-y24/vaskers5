6bcecf8f794d2628d2a7d2d871d7b970d0f5698b
vaskers5 <danil.krokodil@mail.ru>
2021-11-07 23:40:26 +0300

    refactore service

---
+++ IsuExtra.Tests/IsuExtraServiceTest.cs
@@ -0,0 +1,173 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using IsuExtra.Services;
+using IsuExtra.Entities;
+using IsuExtra.Tools;
+using Isu.Services;
+using Isu.Tools;
+
+namespace IsuExtra.Tests
+{
+    public class Tests
+    {
+        private IsuExtraService _isuService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var m3105 = new Group("M3105");
+            var m3104 = new Group("M3104");
+            var Vasiliy = new Student( 311495,"Vasiliy", m3104);
+            var petya = new Student( 311496,"Petya", m3104);
+            var m3104_students = new List<Student>() { Vasiliy, petya };
+            var firstStudent = new Student( 311497,"firstStudent", m3105);
+            var SecondStudent = new Student( 311498,"SecondStudent", m3105);
+            var m3105_students = new List<Student>() { firstStudent, SecondStudent };
+            var groups1 = new List<Group>() { m3104, m3105 };
+            var is1 = new Course(1, groups1);
+            
+            var m3205 = new Group("M3205");
+            var m3204 = new Group("M3204");
+            var vasya2228 = new Student( 312495,"vasya2228", m3204);
+            var petya2 = new Student( 312496,"Petya",m3204);
+            var m3204_students = new List<Student>() { vasya2228, petya2 }; ;
+            var rofl11 = new Student( 312497, "rofl11", m3205);
+            var rofl22 = new Student( 312498, "rofl22", m3205);
+            var m3205_students = new List<Student>() { rofl11, rofl22 };
+            var groups2 = new List<Group>() { m3204, m3205 };
+            var is2 = new Course(2, groups2);
+            var courses = new List<Course>() { is1, is2 };
+
+            var itip = new MegaFaculty('m', "itip");
+            var bit = new MegaFaculty('b', "bit");
+
+            var povyish = new Teacher(0, "Повыш");
+            var hlopotov = new Teacher(1, "Хлопотов");
+            var suslina = new Teacher(2, "Суслина");
+            
+            var audition1 = new Audition(343);
+            var audition2 = new Audition(347);
+            var audition3 = new Audition(348);
+
+            var kiberbez1 = new Lesson("Кибербез", 1, 2, povyish, audition1, itip);
+            var kiberbez2 = new Lesson("Кибербез", 1, 3, povyish, audition1, itip);
+            var informationCulture1 = new Lesson("ЦК", 2, 3, povyish, audition1, itip);
+            var informationCulture2 = new Lesson("ЦК", 2, 4, povyish, audition1, itip);
+            var informationCulture3 = new Lesson("ЦК", 7, 4, povyish, audition1, bit);
+            var matan1 = new Lesson("Матан", 3, 2, suslina, audition1, bit);
+            var matan2 = new Lesson("Матан", 4, 3, suslina, audition2, bit);
+            var matan3 = new Lesson("Матан", 5, 3, suslina, audition3, bit);
+            var matan4 = new Lesson("Матан", 6, 4, suslina, audition1, bit);
+            
+            var timeTable1 = new List<Lesson> {matan2, informationCulture1};
+            var timeTable2 = new List<Lesson> {kiberbez1, matan1};
+            var timeTable3 = new List<Lesson> {matan3, matan4};
+            var timeTable4 = new List<Lesson> {informationCulture1, informationCulture2};
+            var timeTable5 = new List<Lesson> {informationCulture3};
+            var b3304 = new Group("B3304");
+            var vasya_bit = new Student( 312410,"vasya_bit", b3304);
+            var pet_bit = new Student( 312411,"petya_bit", b3304);
+            var bit_students = new List<Student> {vasya_bit, pet_bit};
+            var stream1 = new Stream("1", timeTable1);
+            var stream2 = new Stream("2", timeTable2);
+            var stream3 = new Stream("3", timeTable3);
+            var stream4 = new Stream("4", timeTable4);
+            var stream5 = new Stream("4", timeTable5);
+            
+            var course1 = new EducationCourse("Кибeрбез", itip, new List<Stream>{stream1, stream2});
+            var course2 = new EducationCourse("Матан", bit, new List<Stream>{stream3, stream4, stream5});
+
+            
+            var isuservice = new IsuExtraService( new List<EducationCourse> {course1, course2}, courses);
+
+            foreach (var students in new List<List<Student>>
+                {m3104_students, m3105_students, m3204_students, m3205_students, bit_students})
+            {
+                foreach (var student in students)
+                {
+                    isuservice.AddStudent(student.Group, student.Name);
+                }   
+            }
+
+            var extraM3204 = isuservice.AddGroupTimeTable(m3204, timeTable1);
+            var extraM3205 = isuservice.AddGroupTimeTable(m3205, timeTable2);
+            var extraM3104 = isuservice.AddGroupTimeTable(m3104, timeTable3);
+            var extraM3105 = isuservice.AddGroupTimeTable(m3105, timeTable4);
+            
+            _isuService = isuservice;
+        }
+
+        [Test]
+        public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
+        {
+            Group m3205 = _isuService.FindGroup("M3205");
+            _isuService.AddStudent(m3205, "Sergei Papikyan");
+            Assert.AreEqual(m3205.FindStudent("Sergei Papikyan").Group.GroupName, m3205.GroupName);
+        }
+
+        [Test]
+        public void ReachMaxStudentPerGroup_ThrowException()
+        {
+            Assert.Catch<IsuException>(() =>
+            {
+                var m3205 = _isuService.FindGroup("M3205");
+                _isuService.AddStudent(m3205, "Sergei Papikyan");
+                _isuService.AddStudent(m3205, "Sergei Papikyan1");
+                _isuService.AddStudent(m3205, "Sergei Papikyan2");
+                _isuService.AddStudent(m3205, "Sergei Papikyan3");
+                _isuService.AddStudent(m3205, "Sergei Papikya4");
+                _isuService.AddStudent(m3205, "Sergei Papikya5");
+            });
+        }
+
+        [Test]
+        public void CreateGroupWithInvalidName_ThrowException()
+        {
+            Assert.Catch<IsuException>(() =>
+            {
+                var group = new Group("rofl mem rofl");
+            });
+        }
+        [Test]
+        public void SubscribeStudent_ToHisFaculty_EducationCourse()
+        {
+            Assert.Catch<IsuExtraException>(() =>
+            {
+                EducationCourse course = _isuService.EducationCourses[0];
+                Student papikyan = _isuService.GetStudent(311492);
+                StudentWithTimeTable papikyanBuzy = _isuService.SignUpStudentToCourse(papikyan, course);
+            });
+        }
+        
+        [Test]
+        public void SubscribeStudent_ToTimeTableOverlapsCourse()
+        {
+            Assert.Catch<IsuExtraException>(() =>
+            {
+                EducationCourse course = _isuService.EducationCourses[0];
+                Student papikyan = _isuService.GetStudent(311492);
+                StudentWithTimeTable papikyanBuzy = _isuService.SignUpStudentToCourse(papikyan, course);
+            });
+        }
+        
+        [Test]
+        public void SubscribeStudentToCourse()
+        {
+            EducationCourse course = _isuService.EducationCourses[1];
+            Student papikyan = _isuService.GetStudent(311490);
+            StudentWithTimeTable papikyanBuzy = _isuService.SignUpStudentToCourse(papikyan, course);
+            Assert.AreEqual(papikyanBuzy.OgnpSubjects.Count, 1);
+        }
+        
+        [Test]
+        public void UnsubscribeStudentToEducationCourse()
+        {
+            EducationCourse course = _isuService.EducationCourses[1];
+            Student papikyan = _isuService.GetStudent(311490);
+            StudentWithTimeTable papikyanBuzy = _isuService.SignUpStudentToCourse(papikyan, course);
+            _isuService.UnsubscribeStudentFromCourse(papikyanBuzy, course);
+            Assert.AreEqual(papikyanBuzy.OgnpSubjects.Count, 0);
+        }
+    }
+}
--- IsuExtra/Services/IsuExtraService.cs
+++ IsuExtra/Services/IsuExtraService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -12,13 +13,13 @@ namespace IsuExtra.Services
             : base(courses)
         {
             EducationCourses = educationCourses;
-            GroupsTimeTable = new List<GroupWithTimeTable>() { };
+            GroupsTimeTable = new List<ExtraGroup>() { };
             StudentsTimeTable = new List<StudentWithTimeTable>() { };
         }
 
         public List<EducationCourse> EducationCourses { get; set; }
 
-        private List<GroupWithTimeTable> GroupsTimeTable { get; set; }
+        private List<ExtraGroup> GroupsTimeTable { get; set; }
 
         private List<StudentWithTimeTable> StudentsTimeTable { get; set; }
 
@@ -27,10 +28,10 @@ namespace IsuExtra.Services
             EducationCourses.Add(eduCourse);
         }
 
-        public void SignUpStudentToCourse(Student student, EducationCourse course)
+        public StudentWithTimeTable SignUpStudentToCourse(StudentWithTimeTable student, EducationCourse course)
         {
             List<Stream> streams = CheckGroupTimeTable(student.Group, course);
-            if (streams == null)
+            if (streams.Count == 0)
             {
                 throw new CourseIntersectException("This course overlaps with your group schedule");
             }
@@ -40,36 +41,80 @@ namespace IsuExtra.Services
                     "You are trying to enroll in a course that belongs to your mega-faculty");
             }
 
-            int systemStudentIndex = GetIndexOfStudent(student);
-            foreach (Lesson lesson in streams[0].TimeTable)
-            {
-                StudentsTimeTable[systemStudentIndex].Timetable.Add(lesson);
-            }
+            Stream stream = streams[0];
+            int systemStudentIndex = StudentsTimeTable.IndexOf(student);
+            StudentsTimeTable[systemStudentIndex].Timetable.AddRange(stream.TimeTable);
+
+            stream.AddStudent(student);
+            student.OgnpSubjects.Add(course);
+            return stream.AddStudent(student);
         }
 
-        public void UnsubscribeStudentFromCourse(Student student, EducationCourse course)
+        public StudentWithTimeTable SignUpStudentToCourse(Student student, EducationCourse course)
         {
-            int systemStudentIndex = GetIndexOfStudent(student);
+            return SignUpStudentToCourse(GetStudentWithTimeTable(student), course);
+        }
+
+        public void UnsubscribeStudentFromCourse(StudentWithTimeTable student, EducationCourse course)
+        {
+            int systemStudentIndex = StudentsTimeTable.IndexOf(student);
             var newStudentTimeTable = StudentsTimeTable[systemStudentIndex].Timetable.Where(lesson => lesson.LessonFaculty != course.Faculty).ToList();
+            course.FindStudentStream(student).RemoveStudent(student);
+            student.OgnpSubjects.Remove(course);
             StudentsTimeTable[systemStudentIndex].Timetable = newStudentTimeTable;
+        }
+
+        public IList<StudentWithTimeTable> GetStudents(Stream stream)
+        {
+            return stream.GetStudents();
+        }
+
+        public ExtraGroup AddGroupTimeTable(Group group, List<Lesson> timeTable)
+        {
+            var extraGroup = new ExtraGroup(group.GroupName, group.StudentsList.ToList(), timeTable);
+            AddGroupWithTimeTable(extraGroup);
+            var check = group.StudentsList.Select(student =>
+                new StudentWithTimeTable(student.Id, student.Name, extraGroup));
+            StudentsTimeTable.AddRange(
+                group.StudentsList.Select(student => new StudentWithTimeTable(student.Id, student.Name, extraGroup)));
+            return extraGroup;
+        }
+
+        public List<Student> GetUnsignedStudents(ExtraGroup group)
+        {
+            return group.StudentsList.Where(student =>
+                StudentsTimeTable[GetIndexOfStudent(student)].OgnpSubjects.Count < 2).ToList();
+        }
+
+        public StudentWithTimeTable GetStudentWithTimeTable(Student student)
+        {
+            return StudentsTimeTable[GetIndexOfStudent(student)];
+        }
+
+        private ExtraGroup AddGroupWithTimeTable(ExtraGroup group)
+        {
+            GroupsTimeTable.Add(group);
+            return group;
         }
 
         private List<Stream> CheckGroupTimeTable(Group group, EducationCourse course)
         {
-            var streams = course.GetStreams().Where(stream => stream.IntersectTimeTables(GroupsTimeTable[GetIndexOfGroup(group)].Timetable)).ToList();
+            var streams = course.GetStreams().Where(stream => stream.IntersectTimeTables(GroupsTimeTable[GetIndexOfGroup(group)].TimeTable)).ToList();
             return streams;
         }
 
         private bool CheckCourseFaculty(Group group, MegaFaculty faculty)
         {
-            return faculty.FacultySymbol != group.GroupName;
+            var facSymbol = faculty.FacultySymbol;
+            var groupFac = group.GroupName[0];
+            return char.ToLower(facSymbol) != char.ToLower(groupFac);
         }
 
         private int GetIndexOfStudent(Student sampleStudent)
         {
             for (int index = 0; index < StudentsTimeTable.Count; index++)
             {
-                if (StudentsTimeTable[index].OgnpStudent == sampleStudent)
+                if (StudentsTimeTable[index].Id == sampleStudent.Id)
                     return index;
             }
 
@@ -80,7 +125,7 @@ namespace IsuExtra.Services
         {
             for (int index = 0; index < StudentsTimeTable.Count; index++)
             {
-                if (GroupsTimeTable[index].StudyGroup == sampleGroup)
+                if (GroupsTimeTable[index].GroupName == sampleGroup.GroupName)
                     return index;
             }
 
