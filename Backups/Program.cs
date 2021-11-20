96b7018e175a07b200aed78669b38ca649a80ab9
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-11-21 00:55:03 +0300

    comment local testing

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -8,21 +8,21 @@ namespace Backups
     {
         private static void Main()
         {
-            var service = new BackupsService();
-            string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TestDirectory";
-            var job1 = service.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split);
-
-            service.AddFileToJob(job1, testDirectoryPath + "/a.txt");
-            service.AddFileToJob(job1, testDirectoryPath + "/b.txt");
-            service.AddFileToJob(job1, testDirectoryPath + "/c.txt");
-            try
-            {
-                job1.CreateNewRestorePoint();
-            }
-            catch
-            {
-                Directory.Delete(testDirectoryPath + "/job-1");
-            }
+            // var service = new BackupsService();
+            // string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TestDirectory";
+            // var job1 = service.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split);
+            //
+            // service.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+            // service.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+            // service.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+            // try
+            // {
+            //     job1.CreateNewRestorePoint();
+            // }
+            // catch
+            // {
+            //     Directory.Delete(testDirectoryPath + "/job-1");
+            // }
         }
     }
 }
