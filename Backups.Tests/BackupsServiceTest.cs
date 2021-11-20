8923336de908435fe315dde293a349a196973c21
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-11-20 21:18:19 +0300

    add test

---
+++ Backups.Tests/BackupsServiceTest.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Backups.Entities;
+using Backups.Services;
+using Backups.Tools;
+using NUnit.Framework;
+
+namespace Backups.Tests
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckJobTest()
+        {
+            var service = new BackupsService();
+            string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TesDirectory";
+            var job1 = service.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split);
+            var job2 = service.AddNewJob(testDirectoryPath + "/job-2", BackupAlgorithms.Split);
+            Assert.AreEqual(service.BackupJobs.Count, 2);
+        }
+    }
+}
