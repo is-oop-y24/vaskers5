20fb51b575ff404eb4037324eae083b290d906ac
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-11-20 21:19:03 +0300

    add service

---
+++ Backups/Services/BackupsService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Backups.Entities;
+
+namespace Backups.Services
+{
+    public class BackupsService
+    {
+        public BackupsService()
+        {
+            BackupJobs = new List<BackupJob>() { };
+        }
+
+        public List<BackupJob> BackupJobs { get; set; }
+
+        public BackupJob AddNewJob(string jobPath, BackupAlgorithms algorithm)
+        {
+            Directory.CreateDirectory(jobPath);
+            var job = new BackupJob(jobPath, algorithm);
+            BackupJobs.Add(job);
+            return job;
+        }
+
+        public BackupJob RemoveJob(BackupJob job)
+        {
+            BackupJobs.Remove(job);
+            Directory.Delete(job.JobPath);
+            return job;
+        }
+
+        public SystemFile AddFileToJob(BackupJob job, string filePath)
+        {
+            var file = new SystemFile(filePath, DateTime.Now);
+            job.AddFileToJob(file);
+            return file;
+        }
+    }
+}
