3101ca5bc2a86b0558542c12dbfa8b24aee273fb
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-11-21 00:54:43 +0300

    add new tests

--- Backups.Tests/BackupsServiceTest.cs
+++ Backups.Tests/BackupsServiceTest.cs
@@ -8,21 +8,89 @@ namespace Backups.Tests
 {
     public class Tests
     {
+        private BackupsService _backupsService;
 
         [SetUp]
         public void Setup()
         {
+            _backupsService = new BackupsService();
         }
 
         [Test]
-        public void CheckJobTest()
+        public void CheckJobCreation_VirtualTest()
         {
-            var service = new BackupsService();
-            Assert.AreEqual(1,1);
-            // string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TesDirectory";
-            // var job1 = service.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split);
-            // var job2 = service.AddNewJob(testDirectoryPath + "/job-2", BackupAlgorithms.Split);
-            // Assert.AreEqual(service.BackupJobs.Count, 2);
+            string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TesDirectory";
+            var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split, true);
+            var job2 = _backupsService.AddNewJob(testDirectoryPath + "/job-2", BackupAlgorithms.Split, true);
+            Assert.AreEqual(_backupsService.BackupJobs.Count, 2);
         }
+        
+        [Test]
+        public void Check_SingleFileCreation_VirtualTest()
+        {
+            string testDirectoryPath = "/home/vaskers5/projects/oop/Backups/TestDirectory";
+            var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Single, true);
+            
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+            job1.CreateNewVirtualRestorePoint();
+            Assert.AreEqual(job1.RestorePoints.Count, 1);
+        }
+        
+        [Test]
+        public void Check_SplitFileCreation_VirtualTest()
+        {
+            string testDirectoryPath = "/home/vaskers5/projects/oop/Backups/TestDirectory";
+            var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split, true);
+            
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+            job1.CreateNewVirtualRestorePoint();
+            Assert.AreEqual(job1.RestorePoints.Count, 1);
+        }
+        
+        [Test]
+        public void Check_JobContainsFiles_VirtualTest()
+        {
+            string testDirectoryPath = "/home/vaskers5/projects/oop/Backups/TestDirectory";
+            var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split, true);
+            
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+            _backupsService.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+            
+            Assert.AreEqual(job1.GetBackupFiles().Count, 3);
+        }
+
+
+        // [Test]
+        // public void Check_SingleFileCreation_LocalTest()
+        // {
+        //     string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TestDirectory";
+        //     var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split, false);
+        //
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+        //     job1.CreateNewRestorePoint();
+        //     Assert.AreEqual(job1.RestorePoints.Count, 1);
+        //     Assert.AreEqual(job1.RestorePoints[0].Archives.Count, 1);
+        // }
+        
+        // [Test]
+        // public void Check_SplitFileCreation_LocalTest()
+        // {
+        //     string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TestDirectory";
+        //     var job1 = _backupsService.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split, false);
+        //
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+        //     _backupsService.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+        //     job1.CreateNewRestorePoint();
+        //     Assert.AreEqual(job1.RestorePoints.Count, 1);
+        //     Assert.AreEqual(job1.RestorePoints[0].Archives.Count, 3);
+        // }
     }
 }
