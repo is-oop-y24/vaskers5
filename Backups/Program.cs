ff2d0cadba6eef8bec78576f81bee09482f3d07d
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-11-20 21:18:54 +0300

    add demo-usage

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Backups
+﻿using System.IO;
+using Backups.Entities;
+using Backups.Services;
+
+namespace Backups
 {
     internal class Program
     {
         private static void Main()
         {
+            var service = new BackupsService();
+            string testDirectoryPath = @"/home/vaskers5/projects/oop/Backups/TesDirectory";
+            var job1 = service.AddNewJob(testDirectoryPath + "/job-1", BackupAlgorithms.Split);
+
+        // service.AddFileToJob(job1, testDirectoryPath + "/a.txt");
+        //     service.AddFileToJob(job1, testDirectoryPath + "/b.txt");
+        //     service.AddFileToJob(job1, testDirectoryPath + "/c.txt");
+        //     try
+        //     {
+        //         job1.CreateNewRestorePoint();
+        //     }
+        //     catch
+        //     {
+        //         Directory.Delete(testDirectoryPath + "/job-1");
+        //     }
         }
     }
 }
