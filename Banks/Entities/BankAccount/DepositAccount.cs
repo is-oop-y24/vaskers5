0618b4d66a51ffda6b078b0ba21e9b76ee64342c
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-01 21:30:22 +0300

    add deposit account

---
+++ Banks/Entities/BankAccount/DepositAccount.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Data;
+using Banks.Tools;
+
+namespace Banks.Entities.BankAccount
+{
+    public class DepositAccount : BaseBankAccount
+    {
+        public DepositAccount(
+            int id,
+            Client bankClient,
+            BalanceInterest accountBalanceInterest,
+            BankCommission commission,
+            BankLimit limit,
+            DateTime durationEnd)
+            : base(id, bankClient, accountBalanceInterest, commission, limit)
+        {
+            DurationEnd = durationEnd;
+        }
+
+        public DateTime DurationEnd { get; }
+
+        public override Transaction AddMoneyToAccount(float money)
+        {
+            if (CreationTime < DurationEnd)
+            {
+                throw new DepositAccountDurationException("Your deposit account duration is not end.");
+            }
+            else
+            {
+                return base.AddMoneyToAccount(money);
+            }
+        }
+
+        public override Transaction GetMoneyFromAccount(float money)
+        {
+            if (CreationTime < DurationEnd)
+            {
+                throw new DepositAccountDurationException("Your deposit account duration is not end.");
+            }
+            else
+            {
+                return base.GetMoneyFromAccount(money);
+            }
+        }
+    }
+}
