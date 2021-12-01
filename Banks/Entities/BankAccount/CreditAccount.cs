5c7dd4c00e925433728e2caca9c9c2f8f92d6e8e
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-01 21:30:45 +0300

    add credit account

---
+++ Banks/Entities/BankAccount/CreditAccount.cs
@@ -0,0 +1,38 @@
+using Banks.Tools;
+
+namespace Banks.Entities.BankAccount
+{
+    public class CreditAccount : BaseBankAccount
+    {
+        public CreditAccount(
+            int id,
+            Client bankClient,
+            BalanceInterest accountBalanceInterest,
+            BankCommission commission,
+            BankLimit limit)
+            : base(id, bankClient, accountBalanceInterest, commission, limit)
+        {
+        }
+
+        public override Transaction AddMoneyToAccount(float money)
+        {
+            return Money < 0 ? base.AddMoneyToAccount(money - (Commission.CommissionValue * money)) : base.AddMoneyToAccount(money);
+        }
+
+        public override Transaction GetMoneyFromAccount(float money)
+        {
+            if (Money < 0 && Money > -Limit.LimitValue)
+            {
+                return base.AddMoneyToAccount(money - (Commission.CommissionValue * money));
+            }
+            else if (Money <= -Limit.LimitValue)
+            {
+                throw new LimitException("You have reached your credit limit");
+            }
+            else
+            {
+                return base.GetMoneyFromAccount(money);
+            }
+        }
+    }
+}
