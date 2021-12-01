af8ac9a3baac16024b37296fd27adb2981edab78
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-01 21:30:58 +0300

    add bank base account

---
+++ Banks/Entities/BankAccount/BaseBankAccount.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using Banks.Tools;
+using TransactionException = Banks.Tools.TransactionException;
+
+namespace Banks.Entities.BankAccount
+{
+    public class BaseBankAccount : IBankAccount
+    {
+        private int _lastTransactionId = 0;
+        public BaseBankAccount(int id, Client bankClient, BalanceInterest accountBalanceInterest, BankCommission commission, BankLimit limit)
+        {
+            Id = id;
+            BankClient = bankClient;
+            Money = 0;
+            AccountBalanceInterest = accountBalanceInterest;
+            Commission = commission;
+            Limit = limit;
+            CreationTime = DateTime.Now;
+            TransactionHistory = new List<Transaction>() { };
+        }
+
+        public int Id { get; }
+        public DateTime CreationTime { get; }
+        public Client BankClient { get; }
+        public virtual float Money { get; set; }
+
+        public BalanceInterest AccountBalanceInterest { get; set; }
+        public BankCommission Commission { get; set; }
+        public BankLimit Limit { get; set; }
+        public List<Transaction> TransactionHistory { get; set; }
+
+        public virtual Transaction AddMoneyToAccount(float money)
+        {
+            var transaction = new Transaction(_lastTransactionId++, Money, Money + money);
+            TransactionHistory.Add(transaction);
+            Money += money;
+            return transaction;
+        }
+
+        public virtual Transaction GetMoneyFromAccount(float money)
+        {
+            var transaction = new Transaction(_lastTransactionId++, Money, Money - money);
+            TransactionHistory.Add(transaction);
+            if (BankClient.IsFishy && money > Limit.LimitValue)
+                throw new LimitException("You have exceeded your funds limit");
+            Money -= money;
+            return transaction;
+        }
+
+        public Transaction DeclineTransaction(int id)
+        {
+            Transaction transaction = TransactionHistory.FirstOrDefault(transaction => transaction.Id == id);
+            if (transaction is null)
+            {
+                throw new TransactionException($"We can't find {id} transaction!");
+            }
+            else if (transaction.Declined)
+            {
+                throw new TransactionException($"This transaction already declined!");
+            }
+
+            transaction.Declined = true;
+            Money = transaction.MoneyBefore;
+            return transaction;
+        }
+    }
+}
