01f75955a5afc13e5b59f0a2ba385dfe37e0f3e7
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-05 20:16:21 +0300

    add new tests

--- Banks.Tests/BankServiceTest.cs
+++ Banks.Tests/BankServiceTest.cs
@@ -19,9 +19,6 @@ namespace Banks.Tests
             var centralBank = new CentralBank();
             var bank1 = centralBank.CreateBank("Tinkoff", 3.5f, 4.0f, 1.5f, 50000, 5000000, 5);
             var bank2 = centralBank.CreateBank("Sber", 0, 3.5f, 2.5f, 40000, 5000000, 10);
-            var B1Client1 = bank1.AddBankClient("Misha", false, "Parnas", 1234);
-            var B1Client2 = bank1.AddBankClient("Vika", false);
-            var B2Client1 = bank2.AddBankClient("Misha", true, "Parnas", 1234);
             _centralBank = centralBank;
         }
 
@@ -31,6 +28,70 @@ namespace Banks.Tests
             var bank3 = _centralBank.CreateBank("Alpha", 0, 3.5f, 2.5f, 40000, 5000000, 10);
             Assert.False(bank3 is null);
         }
+        
+        [Test]
+        public void TransferMoneyTest()
+        {
+            var bank1 = _centralBank.FindBankByName("Tinkoff");
+            var B1Client1 = bank1.AddBankClient("Misha", false, "Parnas", 1234);
+            var B1Client1Account = bank1.AddDebitAccountForClient(B1Client1);
+            var B1Client2 = bank1.AddBankClient("Vika", false);
+            var B1Client2Account = bank1.AddDebitAccountForClient(B1Client2);
+            B1Client1Account.AddMoneyToAccount(5000);
+            B1Client2Account.AddMoneyToAccount(10000);
+            bank1.TransferMoney(B1Client1Account.Id, B1Client2Account.Id, 2000);
+            Assert.AreEqual(B1Client1Account.Money, 3000);
+            Assert.AreEqual(B1Client2Account.Money, 12000);
+        }
+        
+        [Test]
+        public void CreditAccountTest()
+        {
+            var bank1 = _centralBank.FindBankByName("Tinkoff");
+            var B1Client1 = bank1.AddBankClient("Misha", false, "Parnas", 1234);
+            var B1Client1Account = bank1.AddCreditAccountForClient(B1Client1);
+            Assert.Catch<BanksException>(() =>
+            {
+                B1Client1Account.GetMoneyFromAccount(10000000);
+            });
+        }
+        
+        [Test]
+        public void FishyAccountTest()
+        {
+            var bank1 = _centralBank.FindBankByName("Tinkoff");
+            var B1Client1 = bank1.AddBankClient("Misha", false);
+            var B1Client1Account = bank1.AddCreditAccountForClient(B1Client1);
+            B1Client1Account.AddMoneyToAccount(600000000);
+            Assert.Catch<BanksException>(() =>
+            {
+                B1Client1Account.GetMoneyFromAccount(50000000);
+            });
+        }
+        
+        [Test]
+        public void DepositAccountTest()
+        {
+            var bank1 = _centralBank.FindBankByName("Tinkoff");
+            var B1Client1 = bank1.AddBankClient("Misha", false);
+            var B1Client1Account = bank1.AddDepositAccountForClient(B1Client1, 5000);
+            Assert.Catch<BanksException>(() =>
+            {
+                B1Client1Account.GetMoneyFromAccount(500);
+            });
+        }
+        [Test]
+        public void DebitAccountTest()
+        {
+            var bank1 = _centralBank.FindBankByName("Tinkoff");
+            var B1Client1 = bank1.AddBankClient("Misha", false);
+            var B1Client1Account = bank1.AddDebitAccountForClient(B1Client1);
+            Assert.Catch<BanksException>(() =>
+            {
+                B1Client1Account.GetMoneyFromAccount(500);
+            });
+        }
+        
     }
         
         
