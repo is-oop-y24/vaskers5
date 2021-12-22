3996334ac7ce2226841f585380dfb27012518751
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-23 00:59:07 +0300

    build bugfix
    This reverts commit 756e065badc056f4b7de872eabd9b7a255e37d06.

---
+++ Banks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Banks.Entities.BankAccount;
+using Banks.Services;
+
+namespace Banks
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
