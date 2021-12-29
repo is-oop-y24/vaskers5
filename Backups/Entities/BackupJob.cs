25f471f113059b15711417c50b95f068e9c1ddcc
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-12-29 21:41:24 +0300

    refactore job

--- Backups/Entities/BackupJob.cs
+++ Backups/Entities/BackupJob.cs
@@ -3,111 +3,57 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using Backups.Entities.Algorithms;
 using Backups.Tools;
 
 namespace Backups.Entities
 {
     public class BackupJob
     {
-        private int _lastId = 0;
-        public BackupJob(string jobPath, BackupAlgorithms algorithm, List<SystemFile> files, bool isVirtual)
+        private int _lastRestorePointId = 0;
+        public BackupJob(string jobPath, IAlgorithm algorithm, ISaver.ISaver saver)
         {
             JobPath = jobPath;
             Algorithm = algorithm;
-            SubDirectoryPath = jobPath + "/" + "SubDirectory";
-            IsVirtual = isVirtual;
-            if (!IsVirtual)
-            {
-                Directory.CreateDirectory(jobPath);
-                Directory.CreateDirectory(SubDirectoryPath);
-            }
-
-            BackupFiles = files;
+            SubDirectoryPath = Path.Combine(jobPath, "SubDirectory");
+            BackupFiles = new List<FileInfo>();
+            Saver = saver;
             RestorePoints = new List<RestorePoint>() { };
         }
 
-        public BackupJob(string jobPath, BackupAlgorithms algorithm, bool isVirtual)
-            : this(jobPath, algorithm, new List<SystemFile>() { }, isVirtual)
-        {
-        }
-
         public string JobPath { get; set; }
-        public BackupAlgorithms Algorithm { get; set; }
+        public IAlgorithm Algorithm { get; set; }
         public List<RestorePoint> RestorePoints { get; set; }
 
-        public bool IsVirtual { get; }
+        public ISaver.ISaver Saver { get; }
         private string SubDirectoryPath { get; }
-        private List<SystemFile> BackupFiles { get; set; }
+        private List<FileInfo> BackupFiles { get; set; }
 
         public virtual RestorePoint CreateNewRestorePoint()
         {
-            string restorePointPath = JobPath + "/RestorePoint_" + _lastId++;
-            BackupFiles.Select(file => file.JustFile.CopyTo(SubDirectoryPath));
-            Directory.CreateDirectory(restorePointPath);
-            RestorePoint restorePoint;
-            switch (Algorithm)
-            {
-                case BackupAlgorithms.Split:
-                    var archives = (from file in BackupFiles
-                        let archPath = restorePointPath + "/" + file.JustFile.Name + ".zip"
-                        select ZipFile.Open(archPath, ZipArchiveMode.Create)
-                            .CreateEntryFromFile(file.JustFile.FullName, archPath)
-                            .Archive).ToList();
-
-                    restorePoint = new RestorePoint(_lastId, restorePointPath,  archives);
-                    break;
-                case BackupAlgorithms.Single:
-                    string zipArchivePath = restorePointPath + "/" + _lastId + ".zip";
-                    ZipFile.CreateFromDirectory(SubDirectoryPath, zipArchivePath);
-                    ZipArchive archive = ZipFile.Open(zipArchivePath, ZipArchiveMode.Read);
-                    restorePoint = new RestorePoint(_lastId, restorePointPath, archive);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            RestorePoints.Add(restorePoint);
-            return restorePoint;
-        }
-
-        public virtual RestorePoint CreateNewVirtualRestorePoint()
-        {
-            string restorePointPath = JobPath + "/RestorePoint_" + _lastId++;
-            RestorePoint restorePoint;
-            switch (Algorithm)
-            {
-                case BackupAlgorithms.Split:
-                    var archives = new List<ZipArchive>() { };
-                    restorePoint = new RestorePoint(_lastId, restorePointPath,  archives);
-                    break;
-                case BackupAlgorithms.Single:
-                    restorePoint = new RestorePoint(_lastId, restorePointPath, new List<ZipArchive>() { });
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            RestorePoints.Add(restorePoint);
-            return restorePoint;
+            var restorePointId = _lastRestorePointId++;
+            var point = new RestorePoint(restorePointId, Path.Combine(SubDirectoryPath, $"{restorePointId}"));
+            Algorithm.MakeBackup(point, BackupFiles, Saver);
+            RestorePoints.Add(point);
+            return point;
         }
 
         public void RemoveRestorePoint(int id)
         {
-            RestorePoint point = RestorePoints.FirstOrDefault(point => point.Id == id);
+            RestorePoint point = RestorePoints.FirstOrDefault(point => point.RestorePointId == id);
             if (point is null)
                 throw new RestorePointNotExistException("This restore point is not exist!");
             Directory.Delete(point.RestorePointPath);
         }
 
-        public string FindFilePathInJob(SystemFile file)
+        public string FindFilePathInJob(FileInfo file)
         {
-            return Directory.GetFiles(SubDirectoryPath).FirstOrDefault(filePath => filePath == file.JustFile.Name);
+            return Directory.GetFiles(SubDirectoryPath).FirstOrDefault(filePath => filePath == file.Name);
         }
 
-        public SystemFile RemoveFileFromJob(SystemFile file)
+        public FileInfo RemoveFileFromJob(FileInfo file)
         {
             BackupFiles.Remove(file);
-            if (IsVirtual) return file;
 
             var jobFile = new FileInfo(FindFilePathInJob(file));
             if (jobFile is null)
@@ -117,15 +63,13 @@ namespace Backups.Entities
             return file;
         }
 
-        public SystemFile AddFileToJob(SystemFile file)
+        public FileInfo AddFileToJob(FileInfo file)
         {
             BackupFiles.Add(file);
-            if (IsVirtual) return file;
-            file.JustFile.CopyTo(SubDirectoryPath + "/" + file.JustFile.Name);
             return file;
         }
 
-        public IReadOnlyCollection<SystemFile> GetBackupFiles()
+        public IReadOnlyCollection<FileInfo> GetBackupFiles()
         {
             return BackupFiles.AsReadOnly();
         }
